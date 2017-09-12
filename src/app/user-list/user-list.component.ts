3e4b7a2a77ef1e88b86a5a2d446e56ff4a2904e8
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-09-13 01:05:14 +0300

    table fix

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,7 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {DataSource} from '@angular/cdk/collections';
-import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/observable/fromEvent';
+
 
 
 @Component({
@@ -11,12 +20,22 @@ import 'rxjs/add/observable/of';
 })
 export class UserListComponent implements OnInit {
 
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns = ['name', 'role', 'email', 'phone', 'branch', 'facility', 'active', 'action' ];
   dataSource = new ExampleDataSource();
+
+  @ViewChild('filter') filter: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    Observable.fromEvent(this.filter.nativeElement, 'keyup')
+      .debounceTime(150)
+      .distinctUntilChanged()
+      .subscribe(() => {
+
+        // if (!this.dataSource) { return; }
+         this.dataSource.filter = this.filter.nativeElement.value;
+      });
   }
 
 }
