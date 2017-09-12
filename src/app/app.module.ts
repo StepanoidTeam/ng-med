5f662103556ab13ab1b82ab1f217e076c20180af
johnnyborjomi <johnnyborjomi@gmail.com>
2017-09-13 00:12:25 +0300

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import {MdToolbarModule,MdIconModule,MdSidenavModule,MdTableModule} from '@angular/material';
+
+
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,NoopAnimationsModule,
+    MdButtonModule, MdCheckboxModule,
+    MdToolbarModule,MdIconModule,
+    MdSidenavModule,MdTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
