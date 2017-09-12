fc95e63c5cb7fd002621faf5b512a5c6fdba83f3
johnnyborjomi <johnnyborjomi@gmail.com>
2017-09-13 00:43:01 +0300

    table changes

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -22,32 +22,25 @@ export class UserListComponent implements OnInit {
 }
 export interface UserRow {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  role: string;
+  email: string;
+  phone: string;
+  branch: string;
+  facility: string;
+  active: boolean;
+  action: string;
 }
 
 const data: UserRow[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+  {name: 'john smith', role: 'user', email:'K.Smith@gmail.com', phone:'(541)-744-3010', branch:'branch', facility:'facility', active: true, action: 'Deactivate'},
+  {name: 'Angelina Joli', role: 'Sister', email:'Angelina@gmail.com', phone:'(341)-744-3010', branch:'branch', facility:'facility', active: false, action: 'Deactivate'},
+  {name: 'Brad Pit', role: 'Brother', email:'Brad@gmail.com', phone:'(941)-744-3010', branch:'branch', facility:'facility', active: true, action: 'Deactivate'},
+  {name: 'john smith', role: 'user', email:'K.Smith@gmail.com', phone:'(541)-744-3010', branch:'branch', facility:'facility', active: true, action: 'Deactivate'},
+  {name: 'Angelina Joli', role: 'Sister', email:'Angelina@gmail.com', phone:'(341)-744-3010', branch:'branch', facility:'facility', active: false, action: 'Deactivate'},
+  {name: 'Brad Pit', role: 'Brother', email:'Brad@gmail.com', phone:'(941)-744-3010', branch:'branch', facility:'facility', active: true, action: 'Deactivate'},
+  {name: 'john smith', role: 'user', email:'K.Smith@gmail.com', phone:'(541)-744-3010', branch:'branch', facility:'facility', active: true, action: 'Deactivate'},
+  {name: 'Angelina Joli', role: 'Sister', email:'Angelina@gmail.com', phone:'(341)-744-3010', branch:'branch', facility:'facility', active: false, action: 'Deactivate'},
+  {name: 'Brad Pit', role: 'Brother', email:'Brad@gmail.com', phone:'(941)-744-3010', branch:'branch', facility:'facility', active: true, action: 'Deactivate'}
 ];
 
 /**
@@ -58,7 +51,7 @@ const data: UserRow[] = [
  */
 export class ExampleDataSource extends DataSource<any> {
   /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<Element[]> {
+  connect(): Observable<UserRow[]> {
     return Observable.of(data);
   }
 
