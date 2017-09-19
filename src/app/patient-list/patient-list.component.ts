09d15bfcbe8b497fd88e37830877c0d8aae22b23
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-09-20 02:35:49 +0300

    patient list impl

--- src/app/patient-list/patient-list.component.ts
+++ src/app/patient-list/patient-list.component.ts
@@ -1,68 +1,80 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { DataSource } from '@angular/cdk/collections';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {DataSource} from '@angular/cdk/collections';
 
 @Component({
-    selector: 'app-patient-list',
-    templateUrl: './patient-list.component.html',
-    styleUrls: ['./patient-list.component.css'],
+  selector: 'app-patient-list',
+  templateUrl: './patient-list.component.html',
+  styleUrls: ['./patient-list.component.css'],
 })
 export class PatientListComponent implements OnInit {
-    displayedColumns = ['name', 'role', 'email', 'phone', 'branch', 'facility', 'active', 'action'];
-    dataSource = new ExampleDataSource();
+  displayedColumns = [
+    'name', 'bloodPressure', 'heartRate', 'weight', 'oxygenLevel', 'lastAlert', 'batteryLevel',
+    'currentDeviceLocation', 'isActive', 'action'];
+  dataSource = new ExampleDataSource();
 
-    @ViewChild('filter') filter: ElementRef;
+  @ViewChild('filter') filter: ElementRef;
 
-    constructor() {}
+  constructor() {
+  }
 
-    ngOnInit() {
-        Observable.fromEvent(this.filter.nativeElement, 'keyup')
-            .debounceTime(150)
-            .distinctUntilChanged()
-            .subscribe(() => {
-                // if (!this.dataSource) { return; }
-                // this.dataSource.filter = this.filter.nativeElement.value;
-            });
-    }
-}
-export interface UserRow {
-    name: string;
-    role: string;
-    email: string;
-    phone: string;
-    branch: string;
-    facility: string;
-    active: boolean;
+  ngOnInit() {
+    Observable.fromEvent(this.filter.nativeElement, 'keyup')
+      .debounceTime(150)
+      .distinctUntilChanged()
+      .subscribe(() => {
+        // if (!this.dataSource) { return; }
+        // this.dataSource.filter = this.filter.nativeElement.value;
+      });
+  }
 }
 
-const data: UserRow[] = [
-    {
-        name: 'John Smith',
-        role: 'Nurse',
-        email: 'K.Smith@gmail.com',
-        phone: '(541)-744-3010',
-        branch: 'dentistry',
-        facility: 'Acute care',
-        active: true,
-    },
-    {
-        name: 'Angelina Joli',
-        role: 'Neurosurgeon',
-        email: 'Angelina@gmail.com',
-        phone: '(341)-744-3010',
-        branch: 'immunology',
-        facility: 'Isolation ward',
-        active: false,
-    },
-    {
-        name: 'Brad Pit',
-        role: 'Cardiologist',
-        email: 'Brad@gmail.com',
-        phone: '(941)-744-3010',
-        branch: 'urology',
-        facility: 'Primary healthcare',
-        active: true,
-    },
+export interface PatientRow {
+  name: string;
+  bloodPressure: string;
+  heartRate: string;
+  weight: string;
+  oxygenLevel: string;
+  lastAlert: string;
+  batteryLevel: number;
+  currentDeviceLocation: string;
+  isActive: boolean;
+}
+
+const data: PatientRow[] = [
+  {
+    name: 'John Smith',
+    bloodPressure: '320/240',
+    heartRate: '54',
+    weight: '43',
+    oxygenLevel: '43',
+    lastAlert: '9/6/2017 1:33 pm',
+    batteryLevel: 76,
+    currentDeviceLocation: 'room A',
+    isActive: true
+  },
+  {
+    name: 'Mark Twen',
+    bloodPressure: '640/480',
+    heartRate: '74',
+    weight: '67',
+    oxygenLevel: '19',
+    lastAlert: '9/6/2017 12:44 pm',
+    batteryLevel: 88,
+    currentDeviceLocation: 'room B',
+    isActive: false
+  },
+  {
+    name: 'John Travolta',
+    bloodPressure: '800/600',
+    heartRate: '73',
+    weight: '81',
+    oxygenLevel: '46',
+    lastAlert: '9/6/2017 11:22 pm',
+    batteryLevel: 20,
+    currentDeviceLocation: 'room C',
+    isActive: false
+  },
 ];
 
 /**
@@ -72,10 +84,11 @@ const data: UserRow[] = [
  * we return a stream that contains only one set of data that doesn't change.
  */
 export class ExampleDataSource extends DataSource<any> {
-    /** Connect function called by the table to retrieve one stream containing the data to render. */
-    connect(): Observable<UserRow[]> {
-        return Observable.of(data);
-    }
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<PatientRow[]> {
+    return Observable.of(data);
+  }
 
-    disconnect() {}
+  disconnect() {
+  }
 }
