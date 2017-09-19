af8521da594a3243922c618ef4ff54660837f242
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-09-20 01:55:12 +0300

    cleanup + new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
 
 import {
   MdButtonModule,
   MdCardModule,
-  MdCheckboxModule, MdChipInput,
+  MdCheckboxModule,
   MdChipsModule,
   MdFormFieldModule,
   MdGridListModule,
@@ -18,85 +18,96 @@ import {
   MdSelectModule,
   MdSidenavModule,
   MdTableModule,
-  MdToolbarModule,
-} from '@angular/material';
+  MdToolbarModule
+} from "@angular/material";
 
-import { AppComponent } from './app.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserDetailsComponent } from './user-details/user-details.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { UnderConstructionComponent } from './under-construction/under-construction.component';
-import { ProfileComponent } from './profile/profile.component';
-import { AlertSettingsComponent } from './alert-settings/alert-settings.component';
-import { AlertDetailsComponent } from './alert-details/alert-details.component';
-import { PatientListComponent } from './patient-list/patient-list.component';
-import { PatientDetailsComponent } from './patient-details/patient-details.component';
-import { FormsModule, NgModel } from '@angular/forms';
+import {AppComponent} from "./app.component";
+import {UserListComponent} from "./user-list/user-list.component";
+import {UserDetailsComponent} from "./user-details/user-details.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+import {UnderConstructionComponent} from "./under-construction/under-construction.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {AlertSettingsComponent} from "./alert-settings/alert-settings.component";
+import {AlertDetailsComponent} from "./alert-details/alert-details.component";
+import {PatientListComponent} from "./patient-list/patient-list.component";
+import {PatientDetailsComponent} from "./patient-details/patient-details.component";
+import {FormsModule} from "@angular/forms";
+import {LoginComponent} from "./login/login.component";
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { PasswordChangeComponent } from './password-change/password-change.component';
 
 const appRoutes: Routes = [
-    { path: 'dashboard', component: PatientListComponent },
+  {path: 'dashboard', component: PatientListComponent},
 
-    { path: 'users', component: UserListComponent },
-    { path: 'users/new', component: UserDetailsComponent },
-    { path: 'users/:id', component: UserDetailsComponent },
+  {path: 'users', component: UserListComponent},
+  {path: 'users/new', component: UserDetailsComponent},
+  {path: 'users/:id', component: UserDetailsComponent},
 
-    { path: 'patients', component: PatientListComponent },
-    { path: 'patients/new', component: PatientDetailsComponent },
-    { path: 'patients/:id', component: PatientDetailsComponent },
+  {path: 'patients', component: PatientListComponent},
+  {path: 'patients/new', component: PatientDetailsComponent},
+  {path: 'patients/:id', component: PatientDetailsComponent},
 
-    { path: 'alerts', component: AlertSettingsComponent },
-    { path: 'alerts/new', component: AlertDetailsComponent },
-    { path: 'alerts/:id', component: AlertDetailsComponent },
+  {path: 'alerts', component: AlertSettingsComponent},
+  {path: 'alerts/new', component: AlertDetailsComponent},
+  {path: 'alerts/:id', component: AlertDetailsComponent},
 
-    { path: 'profile', component: ProfileComponent },
-    { path: 'settings', component: UnderConstructionComponent },
+  {path: 'profile', component: ProfileComponent},
+  {path: 'settings', component: UnderConstructionComponent},
 
-    {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full',
-    },
-    { path: '**', component: NotFoundComponent },
+  {path: 'login', component: LoginComponent},
+  {path: 'password/reset', component: PasswordResetComponent},
+  {path: 'password/change', component: PasswordChangeComponent},
+
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  {path: '**', component: NotFoundComponent},
 ];
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        UserListComponent,
-        UserDetailsComponent,
-        NotFoundComponent,
-        UnderConstructionComponent,
-        ProfileComponent,
-        AlertSettingsComponent,
-        AlertDetailsComponent,
-        PatientListComponent,
-        PatientDetailsComponent,
-    ],
-    imports: [
-        RouterModule.forRoot(
-            appRoutes,
-            // { enableTracing: true } // <-- debugging purposes only
-        ),
-        BrowserModule,
-        NoopAnimationsModule,
-        MdButtonModule,
-        MdCheckboxModule,
-        MdToolbarModule,
-        MdIconModule,
-        MdSidenavModule,
-        MdTableModule,
-        MdFormFieldModule,
-        MdInputModule,
-        MdCardModule,
-        MdListModule,
-        MdPaginatorModule,
-        MdSelectModule,
-        MdGridListModule,
-        MdRadioModule,
-        MdChipsModule,
-        FormsModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    UserListComponent,
+    UserDetailsComponent,
+    NotFoundComponent,
+    UnderConstructionComponent,
+    ProfileComponent,
+    AlertSettingsComponent,
+    AlertDetailsComponent,
+    PatientListComponent,
+    PatientDetailsComponent,
+    LoginComponent,
+    PasswordResetComponent,
+    PasswordChangeComponent,
+  ],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    NoopAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdSidenavModule,
+    MdTableModule,
+    MdFormFieldModule,
+    MdInputModule,
+    MdCardModule,
+    MdListModule,
+    MdPaginatorModule,
+    MdSelectModule,
+    MdGridListModule,
+    MdRadioModule,
+    MdChipsModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
