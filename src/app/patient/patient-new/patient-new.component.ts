f87ff3fdb7371449d2c28e4e361c3a89cf2bd669
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-09-22 03:03:13 +0300

    refac: moving to dirs

--- src/app/patient/patient-new/patient-new.component.ts
+++ src/app/patient/patient-new/patient-new.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface NavTab {
+  label: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-patient-new',
   templateUrl: './patient-new.component.html',
   styleUrls: ['./patient-new.component.css']
 })
 export class PatientNewComponent implements OnInit {
+
+  navTabs: NavTab[] = [
+    {
+      label: 'General Info',
+      link: 'general',
+    },
+    {
+      label: 'Family',
+      link: 'family',
+    }];
 
   constructor() { }
 
