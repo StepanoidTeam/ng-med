228d1618a46018acd70b5929f779fbf10ccf7b7b
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-09-23 02:44:10 +0300

    feature: pendant page

--- src/app/patient/patient-pendant/patient-pendant.component.ts
+++ src/app/patient/patient-pendant/patient-pendant.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {DataSource} from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-patient-pendant',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientPendantComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = [
+    'date', 'location'];
+  dataSource = new ExampleDataSource();
 
-  ngOnInit() {
+  constructor() {
+
   }
 
+  ngOnInit(): void {
+  }
+
+}
+
+export interface PendantRow {
+  date: Date;
+  location: string;
+}
+
+const data: PendantRow[] = [
+  {
+    date: new Date(),
+    location: 'Room A',
+  },
+  {
+    date: new Date(),
+    location: 'Room B',
+  },
+  {
+    date: new Date(),
+    location: 'Room C',
+  },
+];
+
+/**
+ * Data source to provide what data should be rendered in the table. The observable provided
+ * in connect should emit exactly the data that should be rendered by the table. If the data is
+ * altered, the observable should emit that new set of data on the stream. In our case here,
+ * we return a stream that contains only one set of data that doesn't change.
+ */
+export class ExampleDataSource extends DataSource<any> {
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<PendantRow[]> {
+    return Observable.of(data);
+  }
+
+  disconnect() {
+  }
 }
